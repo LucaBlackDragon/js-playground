51f8af9cdee303e31adb4215c5bb2aab1b3b05be
Luca Nardi <l.nardi@pluservice.net>
2020-06-25 08:45:27 +0200

    refactoring scripts per migliorare l'importazione dei tipi

---
+++ userService.d.ts
@@ -0,0 +1,138 @@
+// Type definitions for userService.js
+// Project: JavaScript Playground
+// Definitions by: Luca Nardi <l.nardi@pluservice.net>
+//
+// See also:
+// https://www.typescriptlang.org/docs/handbook/declaration-files/introduction.html
+// https://www.typescriptlang.org/docs/handbook/declaration-files/library-structures.html
+// https://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-function-d-ts.html
+
+export = getUsers;
+
+/**
+ * Restituisce l'elenco degli utenti
+ */
+declare function getUsers(): getUsers.User[];
+// TODO:
+// dopo aver convertito getUsers in una funzione asincrona (come indicato in
+// userService.js) sostituire la dichiarazione con quella seguente:
+// declare function getUsers() : Promise<getUsers.User[]>;
+
+declare namespace getUsers {
+
+  /**
+   * Sesso dell'utente
+   * (come attualmente previsto dalle API di https://randomuser.me)
+   */
+  export type Gender = "male" | "female";
+
+  /**
+   * Dati dell'utente
+   */
+  export interface User {
+    gender: Gender;
+    name: Name;
+    location: Location;
+    email: string;
+    login: Login;
+    dob: BirthInfo;
+    registered: RegistrationInfo;
+    phone: string;
+    cell: string;
+    id: Id;
+    picture: Picture;
+    nat: string;
+  }
+
+  /**
+   * Nome e titolo dell'utente
+   */
+  export interface Name {
+    title: string;
+    first: string;
+    last: string;
+  }
+
+  /**
+   * Indirizzo (via e numero civico)
+   */
+  export interface StreetAddress {
+    number: number;
+    name: string;
+  }
+
+  /**
+   * Coordinate
+   */
+  export interface Coordinates {
+    latitude: string;
+    longitude: string;
+  }
+
+  /**
+   * Timezone dell'utente
+   */
+  export interface Timezone {
+    offset: string;
+    description: string;
+  }
+
+  /**
+   * Indirizzo completo dell'utente
+   */
+  export interface Location {
+    street: StreetAddress;
+    city: string;
+    state: string;
+    country: string;
+    postcode: number;
+    coordinates: Coordinates;
+    timezone: Timezone;
+  }
+
+  /**
+   * Dati per il login
+   */
+  export interface Login {
+    uuid: string;
+    username: string;
+    password: string;
+    salt: string;
+    md5: string;
+    sha1: string;
+    sha256: string;
+  }
+
+  /**
+   * Data di nascita ed età dell'utente
+   */
+  export interface BirthInfo {
+    date: Date;
+    age: number;
+  }
+
+  /**
+   * Data di registrazione ed anni di registrazione dell'utente
+   */
+  export interface Registered {
+    date: Date;
+    age: number;
+  }
+
+  /**
+   * Identificativo univoco dell'utente
+   */
+  export interface Id {
+    name: string;
+    value?: any;
+  }
+
+  /**
+   * Fotografie/avatar dell'utente in tre qualità: large, medium e thumbnail
+   */
+  export interface Picture {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  }
+}
---
+++ userService.js
@@ -0,0 +1,57 @@
+import localUsers from './users.js';
+
+/**
+ * Restituisce l'elenco degli ipotetici utenti della nostra applicazione
+ */
+export default function getUsers() {
+
+  return localUsers;
+
+  // TODO:
+  // convertire questa funzione in una funzione asincrona e richiedere i dati
+  // dalle API effettuando una chiamata http GET a questo URL:
+  // https://randomuser.me/api/?results=5000
+  // invece che importare i dati dal file users.js
+  //
+  // Per le chiamate http, i più coraggiosi possono utilizzare il JavaScript
+  // "puro" ovvero l'antico e terribile oggetto XMLHttpRequest:
+  // https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/
+  //
+  // Oppure si può optare per la più moderna Fetch API introdotta da WHATWG/W3C
+  // (ed implementata ormai da tutti i browser più moderni):
+  // https://developer.mozilla.org/it/docs/Web/API/Fetch_API/Using_Fetch
+  //
+  // Generalmente però si utilizzano librerie ad hoc, ad esempio Axios:
+  // https://github.com/axios/axios
+  // Dopo averla installata utilizzando npm, importarla con l'istruzione:
+  // import axios from 'axios';
+  // (oppure con la sintassi CommonJs suggerita dalla documentazione)
+  // 
+  // NB: Fetch API e Axios (e molte librerie simili) offrono un'API asincrona
+  //     (ovvero basata sulle Promise), mentre l'antico e terribile oggetto
+  //     XMLHttpRequest è ancora basato sul vecchio pattern delle callback
+  //     (questa è solo una delle tante caratteristiche che lo rendono antico e
+  //     terribile), quindi se si sceglie di utilizzarlo bisogna "wrappare" il
+  //     callback pattern con una Promise, ad es:
+  //
+  //     function myCallbackPatternStyleFunctionWrappedInAPromise() {
+  //       
+  //       let resolve, reject;
+  //       const promiseWrapper = new Promise((_resolve, _reject) => {
+  //         resolve = _resolve; 
+  //         reject = _reject; 
+  //       });
+  //       
+  //       function successCallback (result) { resolve(result); }
+  //       function errorCallback (err) { reject(err); }
+  //       
+  //       myCallbackPatternStyledFunction(arg1, arg2, successCallback, errorCallback);
+  //       
+  //       return promiseWrapper;
+  //
+  //     }
+  //
+  // NB2: Una volta resa asincrona la funzione getUsers, aggiornare anche la sua
+  //      interfaccia nel file userService.d.ts (è già pronta e commentata)
+  
+}
